4207a19fb7054dbce8c528d63093e22d16facd0c
Ievgen Gavrysh <ievgen_gavrysh@epam.com>
2019-07-20 07:40:01 -0700

    Complete Assignment, add GreetingRepository

--- src/main/java/guru/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/services/PrimaryGermanGreetingService.java
@@ -9,8 +9,15 @@ import org.springframework.stereotype.Service;
 @Profile("de")
 public class PrimaryGermanGreetingService implements GreetingService {
 
+    private GreetingRepository greetingRepository;
+
+    public PrimaryGermanGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
-        return "Primarer Grubdienst";
+        return greetingRepository.getGermanGreeting();
     }
+
 }
