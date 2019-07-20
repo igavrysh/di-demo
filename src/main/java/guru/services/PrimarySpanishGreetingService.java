232833a11a01a57fd00b9445b6bfdfccd0438026
Ievgen Gavrysh <ievgen_gavrysh@epam.com>
2019-07-20 06:57:05 -0700

    Add project to git, change services locations, introduce @ComponentScan

---
+++ src/main/java/guru/services/PrimarySpanishGreetingService.java
@@ -0,0 +1,16 @@
+package guru.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@Profile("es")
+public class PrimarySpanishGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Servicio de Saludo Primario";
+    }
+}
---
+++ src/main/java/guru/springframework/controllers/SetterInjectedController.java
@@ -0,0 +1,21 @@
+package guru.springframework.controllers;
+
+import guru.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SetterInjectedController {
+
+    private GreetingService greetingService;
+
+    @Autowired
+    public void setGreetingService(@Qualifier("setterGreetingService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+}
